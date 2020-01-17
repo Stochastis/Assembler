0f25c186fca6e2c40c2b6d22701a098771303ba1
cross <cross@192.168.1.1>
2020-01-17 14:33:38 -0600

    Print Hello

---
+++ Playground/src/ui/Main.java
@@ -0,0 +1,21 @@
+package ui;
+/**
+ * 
+ */
+
+/**
+ * @author cross
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
